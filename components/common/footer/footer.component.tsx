7c0293ce872af2000de5c611e846464069b16dc3
stelko <stilyanmtoskov@gmail.com>
2023-06-04 21:34:54 +0100

    footer padding adjusted

--- components/common/footer/footer.component.tsx
+++ components/common/footer/footer.component.tsx
@@ -8,7 +8,7 @@ import Logo from '../logo/logo.component';
 async function Footer() {
   const [navLinks] = await Promise.all([getNavigation()]);
   return (
-    <footer className="bg-black py-16">
+    <footer className="bg-black py-16 md:py-20 xl:py-[4.5rem]">
       <div className="mx-auto flex max-w-xs flex-col gap-10 md:max-w-2xl md:gap-8 xl:max-w-6xl">
         <div className="flex flex-col items-center gap-8 md:flex-row md:justify-between md:border-b md:border-b-white md:border-opacity-10 md:pb-10">
           <Logo />
